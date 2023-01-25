f33f5cbff8e69ff76e902431ff073a8503ad086e
Ketul Patel <patelketul399@gmail.com>
2023-01-25 15:06:09 +0530

    Api

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,20 @@
 import Header from "./Header";
 import Aside from "./Aside";
 import data from "../data";
+import axios from 'axios';
+
 
 function Layout(){
+
+
+  axios.get('https://fakestoreapi.com/products')
+  .then(function (response) {
+    console.log();
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+
   return(
     <>
       <Header />
